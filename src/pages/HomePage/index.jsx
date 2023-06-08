de8233e9225eb48dae1723b5cfc44489d82075de
suelen <sreginaclsilva@gmail.com>
2023-06-08 16:47:39 -0300

    feat: timeline updates

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -6,11 +6,16 @@ import React, { useContext, useEffect, useState } from "react";
 import api from "../../services/api";
 import { LogInContext } from "../../contexts/PersistenLogInContext";
 import Hashtags from "../../components/Hashtags/index.jsx";
+import { BiRefresh } from "react-icons/bi";
+import useInterval from "use-interval";
 
 export default function Home() {
     const { localToken } = useContext(LogInContext);
     const [timelinePosts, setTimelinePosts] = React.useState([]);
     const [reloadTimeline, setReloadTimeline] = useState(false);
+    const [allPosts, setAllPosts] = useState([]);
+    const [displayButton, setDisplayButton] = useState(false)
+    const [postQuantity, setPostQuantity] = useState(0)
 
 
     useEffect(() => {
@@ -21,7 +26,20 @@ export default function Home() {
             .catch(err => console.log(err?.response?.data));
     }, [reloadTimeline]);
 
+    function updatePosts() {
+        const newPosts = allPosts.filter(post => !timelinePosts.some(p => p.id === post.id));
+        if(newPosts.length > 0){
+            setDisplayButton(true);
+            setPostQuantity(newPosts.length);
+        }
+    }
 
+    useInterval(() => api.getPosts(localToken.token)
+        .then(res => {
+            setAllPosts(...res.data)
+            updatePosts();
+        })
+        .catch(err => console.log(err?.response?.data)), 15000);
 
     return (
         <>
@@ -31,6 +49,13 @@ export default function Home() {
                 <ContentContainer>
                     <div>
                         <CardForm reloadTimeline={reloadTimeline} setReloadTimeline={setReloadTimeline} />
+                        <NewPostsButton 
+                        onClick={() => setReloadTimeline(!reloadTimeline)}
+                        data-test="load-btn" 
+                        displayButton={displayButton} >
+                            <p>{postQuantity} new posts, load more!</p>
+                            <Refresh />
+                        </NewPostsButton>
                         {timelinePosts.length !== 0 ? timelinePosts?.map((post, i) => {
                             return (
                                 <PostCard
@@ -86,3 +111,21 @@ const ContentContainer = styled.div`
     display: flex;
     justify-content: space-between;
 `;
+const NewPostsButton = styled.button`
+    width: 38rem;
+    height: 4rem;
+    background-color: #1877F2;
+    color: #FFF;
+    border-radius: 1rem;
+    box-shadow: 0 4px 4px rgba(0 0 0 .25);
+    font-family: 'Lato';
+    font-weight: 400;
+    font-size: 1rem;
+    display: ${props => props.displayButton ?"flex" : none};
+    justify-content: center;
+    align-items: center;
+`;
+const Refresh = styled(BiRefresh)`
+    width: 22px;
+    height: 1rem;
+`
